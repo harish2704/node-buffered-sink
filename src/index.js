de49fe48c9518920eecfb2b12be6da207aea3d6d
Harish.K <harish2704@gmail.com>
2014-09-26 22:36:27 +0530

    first version

---
+++ src/index.js
@@ -0,0 +1,27 @@
+
+function BufferedSink(opts){
+    this.maxSize = opts.maxSize || 10;
+    this.writeItems = opts.writeItems;
+    this.cache = [];
+}
+
+BufferedSink.prototype.flush = function(cb){
+    this.writeItems(this.cache, function(err){
+        if(err){ return cb(err); }
+        this.cache = [];
+        return cb.apply(null, [].slice.call( arguments ) );
+    });
+};
+
+BufferedSink.prototype.write = function(item, cb) {
+    this.cache.push( item );
+    if ( this.cache.length < this.maxSize ){
+        return cb( null );
+    }
+    this.flush( function(err){
+        if(err) { return cb(err); }
+        return cb( null );
+    });
+};
+
+module.exports = BufferedSink;
