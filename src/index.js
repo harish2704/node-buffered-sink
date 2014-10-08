d171e48da0832a0836050ba80fba65c14bc7a6d4
Harish.K <harish2704@gmail.com>
2014-10-08 17:22:29 +0530

    handle write during original writing is takingplace

--- src/index.js
+++ src/index.js
@@ -3,26 +3,36 @@ function BufferedSink(opts){
     this.maxSize = opts.maxSize || 10;
     this.writeItems = opts.writeItems;
     this.cache = [];
+    this.tempCache = [];
+    this.isWriting = false;
 }
 
 BufferedSink.prototype.flush = function(cb){
     var self = this;
+    this.isWriting = true;
     this.writeItems(this.cache, function(err){
         if(err){ return cb(err); }
-        self.cache = [];
+        self.cache = self.tempCache;
+        self.tempCache = [];
+        self.isWriting = false;
         return cb.apply(null, [].slice.call( arguments ) );
     });
 };
 
 BufferedSink.prototype.write = function(item, cb) {
-    this.cache.push( item );
-    if ( this.cache.length < this.maxSize ){
-        return cb( null );
+    if ( this.isWriting ){
+        this.tempCache.push( item );
+        cb( null );
+    } else {
+        this.cache.push( item );
+        if ( this.cache.length < this.maxSize ){
+            return cb( null );
+        }
+        this.flush( function(err){
+            if(err) { return cb(err); }
+            return cb( null );
+        });
     }
-    this.flush( function(err){
-        if(err) { return cb(err); }
-        return cb( null );
-    });
 };
 
 module.exports = BufferedSink;
